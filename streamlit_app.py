b4bd4bdd0b7aadc6517890270315b270a437e78c
Anpatil1 <120495870+Anpatil1@users.noreply.github.com>
2023-05-01 15:52:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,5 +85,7 @@ if st.button('Predict Probability'):
     win = result[0][1]
     st.header(batting_team + "- " + str(round(win*100)) + "%")
     st.header(bowling_team + "- " + str(round(loss*100)) + "%")
-
+        if st.button("About"):
+            st.text("It is created our team ")
+            st.text("Built with Streamlit")
 
